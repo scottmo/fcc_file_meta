41087a0c933939792a88979ff5d4175fde144cbf
System <none>
2017-07-06 03:29:38 +0000

    Working file upload with multer

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 "use strict";
 
 const express = require('express');
+const multer = require('multer');
 const app = express();
+
+const upload = multer({ dest: 'uploads/' });
 
 app.use(express.static('public'));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/file', upload.single('file'), (req, res) => {
+  res.json({ size: req.file.size + " bytes" });
 });
 
 const listener = app.listen(process.env.PORT, function () {
